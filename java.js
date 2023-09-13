7b3bbd7620220690b08788742758acf0b0d6c029
thth0777 <machadothiago162@gmail.com>
2023-09-13 16:59:03 -0300

    menu principal

---
+++ java.js
@@ -0,0 +1,20 @@
+var lista_de_produtos = []
+
+while(true){
+
+    var opcao = prompt(`
+    Bem vindo ao cadastro de produtos! ;)
+    
+    1.Cadastrar Produto
+    2.Editar Produto
+    3.Remover Produto
+    4.Visualizar todos produtos
+    `)
+
+    if (opcao == 1) {
+    }else if (opcao == 2){
+    }else if (opcao == 3){
+    }else if (opcao == 4){
+    }else{alert("nenhuma")}
+}
+
